08023a5bcdd8f4682ba4564a63d09fbecaf256cd
ULTRAcoder666 <ultra.coder666@gmail.com>
2026-06-28 08:00:47 +0300

    fix

--- server/main.py
+++ server/main.py
@@ -31,11 +31,9 @@ from server.api.dependencies import APIContext
 from server.api.routes import router
 from server.config import project_settings
 from server.proxy_server import NTBServer
-from server.tg_bot.handlers import tg_bot_router
 
 tg_bot = Bot(token=project_settings.TG_BOT_TOKEN)
 dp = Dispatcher()
-dp.include_router(tg_bot_router)
 
 
 @asynccontextmanager
